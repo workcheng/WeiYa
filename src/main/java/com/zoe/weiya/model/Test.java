8b94bab005cada0a016bf62178ecf48bd55a7b32
chenghui <chenghui@zysoft.com.cn>
2016-12-15 13:45:53 +0800

    add test object

---
+++ src/main/java/com/zoe/weiya/model/Test.java
@@ -0,0 +1,25 @@
+package com.zoe.weiya.model;
+
+/**
+ * Created by chenghui on 2016/12/15.
+ */
+public class Test {
+    private Integer test_id;
+    private String test_name;
+
+    public Integer getTest_id() {
+        return test_id;
+    }
+
+    public void setTest_id(Integer test_id) {
+        this.test_id = test_id;
+    }
+
+    public String getTest_name() {
+        return test_name;
+    }
+
+    public void setTest_name(String test_name) {
+        this.test_name = test_name;
+    }
+}
