2b527071d1ad713696568f2606da085a75bb5ba6
andy <2864667376@qq.com>
2016-12-18 23:45:13 +0800

    增加js-sdk的配置获取接口

--- src/main/java/com/zoe/weiya/controller/SignController.java
+++ src/main/java/com/zoe/weiya/controller/SignController.java
@@ -1,10 +1,14 @@
 package com.zoe.weiya.controller;
 
+import com.zoe.weiya.comm.constant.CommonConstant;
 import com.zoe.weiya.comm.constant.ZoeErrorCode;
 import com.zoe.weiya.comm.logger.ZoeLogger;
 import com.zoe.weiya.comm.logger.ZoeLoggerFactory;
 import com.zoe.weiya.comm.response.ZoeObject;
 import com.zoe.weiya.model.Area;
+import me.chanjar.weixin.common.exception.WxErrorException;
+import me.chanjar.weixin.mp.api.impl.WxMpServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,12 +21,25 @@ import org.springframework.web.bind.annotation.RestController;
 public class SignController {
     private static final ZoeLogger log = ZoeLoggerFactory.getLogger(SignController.class);
 
+    @Autowired
+    protected WxMpServiceImpl wxMpService;
+
     @RequestMapping(value = "if_over_area",method = RequestMethod.POST)
     public Object ifOverArea(Area area){
         //TODO 判断位置范围，返回是否在这个范围当中
         if(true){
             return ZoeObject.success(ZoeErrorCode.SUCCESS);
         }else{
+            return ZoeObject.failure(ZoeErrorCode.ERROR_WECHAT);
+        }
+    }
+
+    @RequestMapping("url")
+    public Object getSign(String url){
+        try {
+            return ZoeObject.success(wxMpService.createJsapiSignature(url));
+        } catch (WxErrorException e) {
+            log.error(CommonConstant.ERROR+e.toString());
             return ZoeObject.failure(ZoeErrorCode.ERROR_WECHAT);
         }
     }
---
+++ src/test/java/com/zoe/weiya/controller/SignControllerTest.java
@@ -0,0 +1,14 @@
+package com.zoe.weiya.controller;
+
+import org.junit.Test;
+
+/**
+ * Created by andy on 2016/12/18.
+ */
+public class SignControllerTest {
+    @Test
+    public void getSign() throws Exception {
+
+    }
+
+}
