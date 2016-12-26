833063c3d6bc67bdb8a38a3514943e23bd811556
andy <2864667376@qq.com>
2016-12-26 23:03:27 +0800

    去掉mysql，mybatis相关引用，修正单词拼写错误

--- src/main/java/com/zoe/weiya/dao/BaseDaoImpl.java
+++ src/main/java/com/zoe/weiya/dao/BaseDaoImpl.java
@@ -1,10 +1,6 @@
 package com.zoe.weiya.dao;
 
-import org.mybatis.spring.SqlSessionTemplate;
-
-import javax.annotation.Resource;
-
 public class BaseDaoImpl {
-  @Resource(name = "sqlSessionTemplate")
-  protected  SqlSessionTemplate   sqlSessionTemplate;
+//  @Resource(name = "sqlSessionTemplate")
+//  protected  SqlSessionTemplate   sqlSessionTemplate;
 }
--- src/main/java/com/zoe/weiya/service/ZoeTestService.java
+++ src/main/java/com/zoe/weiya/service/ZoeTestService.java
@@ -1,20 +1,7 @@
 package com.zoe.weiya.service;
 
-import com.zoe.weiya.dao.test.TestDao;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import java.sql.SQLException;
-
 /**
  * Created by andy on 2016/12/14.
  */
-@Service
 public class ZoeTestService {
-    @Autowired
-    private TestDao testDao;
-
-    public Object getTest() throws SQLException {
-        return testDao.getTest();
-    }
 }
