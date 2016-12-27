f5c2bf7f98bc9c631ce6ac4d75fd0115050ace20
andy <2864667376@qq.com>
2016-12-27 22:58:13 +0800

    更改日志输出级别

--- src/main/java/com/zoe/weiya/interceptor/time/TimeBasedAccessInterceptor.java
+++ src/main/java/com/zoe/weiya/interceptor/time/TimeBasedAccessInterceptor.java
@@ -38,8 +38,8 @@ public class TimeBasedAccessInterceptor extends HandlerInterceptorAdapter {
     public boolean preHandle(HttpServletRequest request,
                              HttpServletResponse response, Object handler) throws Exception {
         String url = request.getRequestURL().toString();
-        log.error("url=" + url);
-        log.error("mappingURL=" + mappingURL);
+        log.info("url=" + url);
+        log.info("mappingURL=" + mappingURL);
         if (mappingURL == null || url.matches(mappingURL)) {//如果匹配url，是所要控制的页面
             Calendar c = Calendar.getInstance();
             c.setTime(new Date());
