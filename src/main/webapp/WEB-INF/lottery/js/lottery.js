246811277b853f984f91b1e8af13400fb8238172
zxc <1053368376@qq.com>
2017-01-05 20:28:25 +0800

    弹幕

--- src/main/webapp/WEB-INF/lottery/js/lottery.js
+++ src/main/webapp/WEB-INF/lottery/js/lottery.js
@@ -1,11 +1,11 @@
 var getAllUserInfo = [];
 var luckyUser = null;
 var setintIndex = 0;
-function getRandom(min, max) {
+
+var getRandom = function (min, max) {
     var r = Math.random() * (max - min);
     var re = Math.round(r + min);
     re = Math.max(Math.min(re, max), min)
-
     return re;
 }
 //获取用户
@@ -21,34 +21,29 @@ var getLottery = function () {
     })
 }
 
-
 var isAuto = function () {
 
     setintIndex = setInterval(function () {
         var user_index = getRandom(0, 500);
-
         $("#userName").html(getAllUserInfo[user_index].name);
         $("#userImg").attr("src", getAllUserInfo[user_index].headImgUrl);
-
     }, 100);
 
     beginLuck();
-    $('#lottery .condition').addClass('disabled');
+    $('.condition').addClass('disabled');
     return false;
 }
-var beginLuck = function () {  //key 0:只抽一个人奖 1:自动抽奖
-
+var beginLuck = function () {
     $("#stopLuck").show();
     $("#beginLuck").hide();
-
-
 }
 
-//停止抽奖
+/**
+ * 停止抽奖
+ * @returns {boolean}
+ */
 var stopLuck = function () {
-    //
-    clearInterval(setintIndex);
-//后台选择
+
     var luckyLevel = $("select[name='prize']").val();
     var userNum = $("select[name='userNum']").val();
     var getLotteryUser = BaseUrl + "user/lotterySelect";
@@ -62,16 +57,8 @@ var stopLuck = function () {
             var userName = luckyUser.name;
             var imgUl = luckyUser.headImgUrl;
             showLuckAnimate(imgUl, luckyLevel, userName);
-            var listContent = $("<li></li>").addClass("list-content clearfix");
-            var listIdx = $("<div></div>").addClass("list-idx").text(1);
-            var listUser = $("<div></div>").addClass("list-user");
-            var jqImg = $("<img>").attr("src", imgUl);
-            var jqName = $("<p></p>").addClass("text-overflow").text(userName);
-            var jqPrize = $("<div></div>").addClass("list-prize-name text-overflow").text(luckyLevel);
-            listUser.append(jqImg).append(jqName);
-            listContent.append(listIdx).append(listUser).append(jqPrize);
-
-            $("#luckyUser").find("ul").prepend(listContent);
+            clearInterval(setintIndex);
+            showLuckyUser(1, imgUl, userName, luckyLevel);
         }
     })
 
@@ -88,9 +75,32 @@ var stopLuck = function () {
     $("#stopLuck").hide();
     return false;
 }
+/**
+ * 获奖名单
+ * @param idx
+ * @param imgUI
+ * @param userName
+ * @param luckyLevel
+ */
+var showLuckyUser = function (idx, imgUI, userName, luckyLevel) {
+    var listContent = $("<li></li>").addClass("list-content clearfix");
+    var listIdx = $("<div></div>").addClass("list-idx").text(idx);
+    var listUser = $("<div></div>").addClass("list-user");
+    var jqImg = $("<img>").attr("src", imgUI);
+    var jqName = $("<p></p>").addClass("text-overflow").text(userName);
+    var listPrize = $("<div></div>").addClass("list-prize text-overflow");
+    var jqPrize = $("<div></div>").addClass("list-prize-name text-overflow").text(luckyLevel);
+    listUser.append(jqImg).append(jqName);
+    listContent.append(listIdx).append(listUser).append(listPrize).append(jqPrize);
+    $("#luckyUser").find("ul").prepend(listContent);
+}
 
-
-//显示抽奖动画
+/**
+ * 显示抽奖动画
+ * @param imgUl
+ * @param showLevel
+ * @param userName
+ */
 var showLuckAnimate = function (imgUl, showLevel, userName) {
     $('body').append('<div class="animate-bg"><div class="light"></div><img class="luckbg" src="images/luckbg.png" /><img src="' + imgUl + '" class="luckUserHead" /><div class="showLuckUserName">' + userName + '</div><div class="showLuckLevel">恭喜<span>' + userName + '</span> 获<span>' + showLevel + '</span>！</div></div>');
     $(".luckbg").animate({
