a2f661f348d6074a7583dc6545e2a73a4b0db119
zxc <1053368376@qq.com>
2017-01-07 21:42:22 +0800

    评论上墙

--- src/main/webapp/WEB-INF/lottery/js/config.js
+++ src/main/webapp/WEB-INF/lottery/js/config.js
@@ -1,4 +1,4 @@
 /**
  * Created by zhangxingcai on 2016/12/26 0026.
  */
-BaseUrl = "http://zxc.tunnel.qydev.com/";//"http://localhost:8068/";
+BaseUrl = "http://localhost:8080/";//"http://localhost:8068/";
