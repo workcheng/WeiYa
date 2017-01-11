339f0b07f33e764c7810571015ac7a08db8e8530
zxc <1053368376@qq.com>
2017-01-11 16:09:22 +0800

    界面优化

--- src/main/webapp/WEB-INF/comment/js/jquery.qqFace.js
+++ src/main/webapp/WEB-INF/comment/js/jquery.qqFace.js
@@ -26,7 +26,7 @@
                 for (var i = 1; i <= 75; i++) {
                     labFace = '[' + tip + i + ']';
                     strFace += '<td><img src="' + path + i + '.gif" onclick="$(\'#' + option.assign + '\').setCaret();$(\'#' + option.assign + '\').insertAtCaret(\'' + labFace + '\');" /></td>';
-                    if (i % 12 == 0) strFace += '</tr><tr>';
+                    if (i % 10 == 0) strFace += '</tr><tr>';
                 }
                 strFace += '</tr></table></div>';
             }
@@ -34,7 +34,7 @@
             var offset = $(this).position();
             var top = offset.top + $(this).outerHeight();
             $('#' + id).css('top', 100);
-            $('#' + id).css('left', 12);
+            $('#' + id).css('left', 10);
             $('#' + id).show();
             e.stopPropagation();
         });
