bed35157113c90427ba92b6f0fa0fe362cdf9da6
chenghui <chenghui@zysoft.com.cn>
2017-01-13 10:46:31 +0800

    增加测试

---
+++ src/test/java/com/zoe/weiya/util/ZoeUtilTest.java
@@ -0,0 +1,14 @@
+package com.zoe.weiya.util;
+
+import org.junit.Test;
+
+/**
+ * Created by chenghui on 2017/1/13.
+ */
+public class ZoeUtilTest {
+
+    @Test
+    public void testGetIndex() throws Exception {
+        System.out.println(ZoeUtil.getIndex());
+    }
+}
