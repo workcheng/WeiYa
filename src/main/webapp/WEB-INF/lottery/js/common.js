3b41906c5dd9dea9b423291d3a5d41f9a90897a7
zxc <1053368376@qq.com>
2017-01-16 14:58:19 +0800

    Merge remote-tracking branch 'origin/ch' into zxc
    Conflicts:
    	src/main/webapp/WEB-INF/comment/css/style.css

--- src/main/webapp/WEB-INF/lottery/js/common.js
+++ src/main/webapp/WEB-INF/lottery/js/common.js
@@ -72,3 +72,11 @@ var getNowFormatDate = function () {
     var currentdate = year + "-" + month + "-" + strDate + " " + hour + ":" + second + ":" + minute;
     return currentdate;
 }
+
+var replace_em = function (str) {
+    str = str.replace(/\</g, '&lt;');
+    str = str.replace(/\>/g, '&gt;');
+    str = str.replace(/\n/g, '<br/>');
+    str = str.replace(/\[em_([0-9]*)\]/g, '<img src="../comment/arclist/$1.gif" border="0" />');
+    return str;
+}
