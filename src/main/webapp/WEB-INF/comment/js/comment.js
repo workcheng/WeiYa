0c5a3b9084789724ef5b2349a666bae93221c98f
zxc <1053368376@qq.com>
2017-01-19 20:35:03 +0800

    完善

--- src/main/webapp/WEB-INF/comment/js/comment.js
+++ src/main/webapp/WEB-INF/comment/js/comment.js
@@ -2,52 +2,51 @@
  * Created by zxc on 2017/1/8.
  */
 $(document).ready(function () {
-    var weixinUrl = location.href.split('#')[0];
-    var url = BaseUrl + "sign/url";
+    /*var weixinUrl = location.href.split('#')[0];
+     var url = BaseUrl + "sign/url";
 
 
-    // 当前的网页请求地址
+     // 当前的网页请求地址
+     $.ajax({
+     url: url, //这个地址是服务器配置JSSDK的地址
+     data: {           // 这个地址是发生jssdk调用的url地址,用于服务器配置
+     url: weixinUrl
+     },
+     success: function (json) {
+     var data = json.data;
+     var config = {};
+     for (var k in json.data) {
+     config[k] = json.data[k];
+     }
+     config.debug = false;// 添加你需要的JSSDK的权限
+     config.appId = config.appid;
+     config.jsApiList = ['hideOptionMenu'];
+     config.timestamp = parseInt(config.timestamp);
+     config.nonceStr = config.noncestr;
+     config.signature = config.signature
+     wx.config(config);
+     wx.ready(function () {
+     wx.hideOptionMenu();*/
+    var getUserInfo = BaseUrl + "sign/getUserInfo";
+    var weixin = location.href.split('?')[1];
+    var code = weixin.split('=')[1];
+    //获取用户信息
     $.ajax({
-        url: url, //这个地址是服务器配置JSSDK的地址
-        data: {           // 这个地址是发生jssdk调用的url地址,用于服务器配置
-            url: weixinUrl
+        url: getUserInfo, //这个地址是服务器配置JSSDK的地址
+        data: {           // 这个地址是发生jssdk调用的url地址
+            // 用于服务器配置
+            id: code
         },
         success: function (json) {
-            var data = json.data;
-            var config = {};
-            for (var k in json.data) {
-                config[k] = json.data[k];
-            }
-            config.debug = false;// 添加你需要的JSSDK的权限
-            config.appId = config.appid;
-            config.jsApiList = ['hideOptionMenu'];
-            config.timestamp = parseInt(config.timestamp);
-            config.nonceStr = config.noncestr;
-            config.signature = config.signature
-            wx.config(config);
-            wx.ready(function () {
-                wx.hideOptionMenu();
-                var getUserInfo = BaseUrl + "sign/getUserInfo";
-                var weixin = location.href.split('?')[1];
-                var code = weixin.split('=')[1];
-                //获取用户信息
-                $.ajax({
-                    url: getUserInfo, //这个地址是服务器配置JSSDK的地址
-                    data: {           // 这个地址是发生jssdk调用的url地址
-                        // 用于服务器配置
-                        id: code,
-                        lang: ''
-                    },
-                    success: function (json) {
-                        var userInfo = json.data;
-                        $("#headImgUrl").attr("src", userInfo.headImgUrl);
-                        $("#nickName").text(userInfo.nickname);
-                        saveMsgClick(userInfo.headImgUrl);
-                    }
-                })
-            });
+            var userInfo = json.data;
+            $("#headImgUrl").attr("src", userInfo.headImgUrl);
+            $("#nickName").text(userInfo.nickname);
+            saveMsgClick(userInfo.headImgUrl);
         }
     })
+    // });
+    /*    }
+     })*/
     /*$('.emotion').qqFace({
      id: 'facebox',
      assign: 'msgDanmu',
